bb315477945c15894973bda20df29de5d748f5e3
Dharma.K <dharmakrish6@gmail.com>
2021-06-13 10:04:50 +0530

    Update dev_pricein.py

--- dev_pricein.py
+++ dev_pricein.py
@@ -11,7 +11,7 @@ headers = {
   'Content-Type': 'application/json'
 }
 def pushData(data):
-    return requests.request("POST", url, headers=headers, data=json.dumps(data))
+    return requests.request("PUT", url, headers=headers, data=json.dumps(data))
 
 markets=["b94654e3-b04d-4f8c-a6df-466dfca14bb1","e39466a1-f557-4e5f-a9fa-e00868e8dee4","6c75a7eb-3df8-4ec5-acf2-0e50068bee12","46c6a570-a1ac-48cd-8e1f-0945a1e535f3","cdef737f-34b1-43c8-a320-7f2c6556a467"]
 for market in markets:
